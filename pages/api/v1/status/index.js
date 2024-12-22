ed3a0646111b3559b28b8d9d43ae637916b02a4d
Gustavo Freitas <freitasgustavos@hotmail.com>
2024-12-22 16:14:32 -0300

    adicionado arquivos `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT NOW()");
+  console.log({ result });
   response.json({ status: "ok" });
 }
 
